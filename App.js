8bbcedcc15abf7cb337bd20398ab793d4f03c328
Danko Milutinovic <dankomilutinovic@gmail.com>
2017-12-05 12:42:17 +0100

    adding drawer nav with two screens

--- App.js
+++ App.js
@@ -1,37 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import { Constants, Svg } from 'expo';
 
-export default class App extends React.Component {
+import {
+  DrawerNavigator,
+} from 'react-navigation';
+
+class MyHomeScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Home',
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
-        <Svg 
-          x={0}
-          y={0}
-          width={100} 
-          height={100}
-          viewBox="0 0 300 300"
-          style={{marginVertical: 10}}
-        >
-          <Svg.Rect
-            x="0"
-            y="0"
-            rx="15" 
-            ry="15"
-            height="300"
-            width="300"
-            fill="#4265f4"
-          />
-          <Svg.Path            
-            d="m 250,87.974 c -7.358,3.264 -15.267,5.469 -23.566,6.461 8.471,-5.078 14.978,-13.119 18.041,-22.701 -7.929,4.703 -16.71,8.117 -26.057,9.957 -7.484,-7.975 -18.148,-12.957 -29.95,-12.957 -22.66,0 -41.033,18.371 -41.033,41.031 0,3.216 0.363,6.348 1.062,9.351 -34.102,-1.711 -64.336,-18.047 -84.574,-42.872 -3.532,6.06 -5.556,13.108 -5.556,20.628 0,14.236 7.244,26.795 18.254,34.153 -6.726,-0.213 -13.053,-2.059 -18.585,-5.132 -0.004,0.171 -0.004,0.343 -0.004,0.516 0,19.88 14.144,36.464 32.915,40.234 -3.443,0.938 -7.068,1.439 -10.81,1.439 -2.644,0 -5.214,-0.258 -7.72,-0.736 5.222,16.301 20.375,28.165 38.331,28.495 -14.043,11.006 -31.735,17.565 -50.96,17.565 -3.312,0 -6.578,-0.194 -9.788,-0.574 18.159,11.643 39.727,18.437 62.899,18.437 75.473,0 116.746,-62.524 116.746,-116.747 0,-1.779 -0.04,-3.548 -0.119,-5.309 8.017,-5.784 14.973,-13.011 20.474,-21.239 z"
-            fill="#3aaae1"
-            fill="#fff"
-          />  
-        </Svg>
-        <Text>Twiter</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('Notifications')}
+          title="Go to notifications"
+        />
+        <Text>You are on the home screen</Text>
+      </View>  
+    );
+  }
+}
+
+class MyNotificationsScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Notifications',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={() => this.props.navigation.goBack()}
+          title="Go back home"
+        />
+        <Text>You are on the notifications screen</Text>
       </View>
     );
   }
@@ -40,8 +46,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  icon: {
+    width: 24,
+    height: 24,
   },
 });
+
+let App = DrawerNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+});
+
+
+export default App
